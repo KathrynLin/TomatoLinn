3a69004812c74cdbb866eae343f94489f874849f
林方晴 <linfangqing@linfangqingdeMacBook-Air-2.local>
2024-04-18 12:24:06 -0400

    first commit

---
+++ js/resume.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all the icons and contents
+    const icons = document.querySelectorAll('.timeline-icon');
+    const contents = document.querySelectorAll('.timeline-content');
+
+    // Function to toggle the display of content
+    function toggleContent(index) {
+        contents[index].classList.toggle('active');
+    }
+
+    // Add click event listener to icons
+    icons.forEach((icon, index) => {
+        icon.addEventListener('click', function() {
+            toggleContent(index);
+        });
+    });
+});
