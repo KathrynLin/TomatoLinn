c7058aaee81e2718577f9e7d50d7e1fbfd42ac14
林方晴 <linfangqing@linfangqingdeMacBook-Air-2.local>
2024-04-21 00:02:33 -0400

    change resume

--- js/resume.js
+++ js/resume.js
@@ -15,3 +15,22 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all the icons and contents
+  const icons = document.querySelectorAll('.experience-icon');
+  const contents = document.querySelectorAll('#experience-content');
+
+  // Function to toggle the display of content
+  function toggleContent(index) {
+      contents[index].classList.toggle('active');
+  }
+
+  // Add click event listener to icons
+  icons.forEach((icon, index) => {
+      icon.addEventListener('click', function() {
+          toggleContent(index);
+      });
+  });
+});
+  
